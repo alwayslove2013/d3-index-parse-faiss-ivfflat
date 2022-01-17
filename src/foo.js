5728386a2c0083f7a88c9b7504925c7502169a8c
min.tian <min.tian.cn@gmail.com>
2022-01-17 16:53:18 +0800

    remove yarn

--- src/foo.js
+++ src/foo.js
@@ -1,3 +1,8 @@
-export default function() {
-  return 42;
+// import fetch from "d3-fetch";
+
+const indexParser = (url) => {
+  console.log("url", url);
+  return 40;
 };
+
+export default indexParser;
--- test/foo-test.js
+++ test/foo-test.js
@@ -2,6 +2,6 @@ var tape = require("tape"),
     foo = require("../");
 
 tape("foo() returns the answer to the ultimate question of life, the universe, and everything.", function(test) {
-  test.equal(foo.foo(), 42);
+  test.equal(foo.foo(), 40);
   test.end();
 });
