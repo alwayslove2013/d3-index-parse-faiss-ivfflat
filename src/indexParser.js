ad1c7f009b278d854e6057c84f2adb8ecbcc14d0
min.tian <min.tian.cn@gmail.com>
2022-01-19 18:35:42 +0800

    read ivfflat

--- src/indexParser.js
+++ src/indexParser.js
@@ -63,6 +63,15 @@ class MiniIndex {
     this.p += 8;
     return float64;
   }
+  readFloat32Array(n) {
+    const res = new Float32Array(this.data.slice(this.p, this.p + n * 4));
+    this.p += n * 4;
+    return res;
+  }
+  readInt64Array(n) {
+    const res = generateArray(n).map((_) => this.readInt64());
+    return res;
+  }
   readDummy() {
     const dummy = this.readInt64();
     return dummy;
@@ -95,11 +104,13 @@ class MiniIndex {
     directMap.dmType = dmTypeNum;
     directMap._dmType = dmType;
     if (
-      dmTypeNum !== DirectMapType.NoMap &&
-      dmTypeNum !== DirectMapType.Array
+      dmTypeNum !== DirectMapType.NoMap
+      // && dmTypeNum !== DirectMapType.Array
     ) {
-      console.warn("directmap type only supports NoMap or Array.");
+      console.warn("directmap type only supports NoMap.");
     }
+
+    directMap.size = this.readInt64();
 
     // directMap.array = new BigInt64Array(
     //   this.data.slice(this.p, this.p + index.ntotal * 8)
@@ -121,17 +132,52 @@ class MiniIndex {
     const size = this.readInt64();
     index._codesSize = size;
 
-    // const codes = generateArray(index.ntotal).map(
-    //   (_, i) =>
-    //     new Float64Array(
-    //       this.data.slice(this.p + i * 8 * 64, this.p + (i + 1) * 8 * 64)
-    //     )
-    // );
-    // const codes = new Uint8Array(this.data.slice(this.p, this.p + size * 4))
-    const codes = new Float32Array(this.data.slice(this.p, this.p + size * 4));
-    console.log(codes.filter((a, i) => i % 64 === 0).sort());
-    this.p += size * 4;
+    // const codes = new Float32Array(this.data.slice(this.p, this.p + size * 4));
+    // // console.log(codes.filter((_, i) => i % index.d === 0).sort());
+    // this.p += size * 4;
+    const codes = generateArray(index.ntotal).map(_ => this.readFloat32Array(index.d))
+    
     index.codes = codes;
+  }
+  readArrayInvertedListsSizes(inv) {
+    inv.listType = this.readH();
+    if (inv.listType !== "full") {
+      console.warn("inv listType only supports full, not sprs");
+    }
+    inv.listSizeSize = this.readInt64();
+    const listSizes = generateArray(inv.listSizeSize).map((_) =>
+      this.readInt64()
+    );
+    inv.listSizes = listSizes;
+    const ids = [];
+    const codes = [];
+    for (let i = 0; i < inv.nlist; i++) {
+      codes.push(
+        generateArray(listSizes[i]).map((_) =>
+          this.readFloat32Array(inv.codeSize / 4)
+        )
+      );
+      ids.push(this.readInt64Array(listSizes[i]));
+    }
+    // console.log(ids[0])
+    // console.log(codes[0])
+    inv.ids = ids;
+    inv.codes = codes;
+  }
+  readInvertedLists(index) {
+    // ivf->invlists = ils;
+    const inv = {};
+    inv.h = this.readH();
+    console.log('h', inv.h)
+    if (inv.h !== "ilar") {
+      console.warn("invlist wrong, ", inv.h);
+    }
+    inv.nlist = this.readInt64();
+    inv.codeSize = this.readInt64();
+
+    this.readArrayInvertedListsSizes(inv);
+
+    index.inv = inv;
   }
   readIndex(index) {
     const h = this.readH();
@@ -139,6 +185,10 @@ class MiniIndex {
     index.h = h;
     if (h === "IwFl") {
       this.readIvfHeader(index);
+      index.codeSize = index.d * 32;
+      this.readInvertedLists(index);
+
+      console.log('Finished check:', this.p === this.data.byteLength)
     } else if (h === "IxF2" || h === "IxFI") {
       this.readIndexHeader(index);
       this.readXbVectors(index);
